89d63fb58972920c676d0e9b4d08b263620ab7c7
jcy97 <sook3427@naver.com>
2025-01-03 14:49:24 +0900

    [feat] express 웹서버 설정

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
